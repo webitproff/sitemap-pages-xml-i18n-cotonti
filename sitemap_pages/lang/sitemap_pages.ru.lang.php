74718c119eac30b4b7b2f55c12683adbf2af9716
webitproff <webitproff@gmail.com>
2026-08-05 21:07:02 +0300

    Revise plugin URI, date, and descriptions
    Updated plugin information and descriptions for clarity.

--- sitemap_pages/lang/sitemap_pages.ru.lang.php
+++ sitemap_pages/lang/sitemap_pages.ru.lang.php
@@ -10,12 +10,13 @@
  *
  * Filename: plugins/sitemap_pages/lang/sitemap_pages.ru.lang.php
  *
- * Plugin URI:  https://abuyfile.com/ru/page/cotonti/plugs/sitemap-pages-xml-i18n
+ * Plugin URI:  https://abuyfile.com/ru/market/cotonti/plugs/sitemap-pages-xml-i18n
+ * Support:     https://abuyfile.com/forums/cotonti/custom/plugs/
  * Source:      https://github.com/webitproff/sitemap-pages-xml-i18n-cotonti
  *
- * Date: Aug 3, 2026
+ * Date: Aug 5, 2026
  * @package sitemap_pages
- * @version 1.0.0
+ * @version 1.1.1
  * @author webitproff
  * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff/sitemap-pages-xml-i18n-cotonti
  * @license BSD
@@ -26,9 +27,15 @@ defined('COT_CODE') or die('Wrong URL.');
 // ========================
 // ИНФОРМАЦИЯ О ПЛАГИНЕ (АДМИНКА)
 // ========================
-$L['info_name']  = 'Sitemap Pages (мультиязычная карта страниц)';
-$L['info_desc']  = 'Генерирует XML-карту сайта только для модуля Page с поддержкой разных языков.';
-$L['info_notes'] = 'После установки добавьте правила в .htaccess и пропишите Sitemap в robots.txt (подробности в документации).';
+$L['info_name']  = 'Sitemap Pages i18n';
+$L['info_desc']  = 'Генерирует XML-карту сайта только для модуля Page с поддержкой плагина i18n - разных языков.';
+$L['info_notes'] = 'После установки добавить/проверить правила в .htaccess и robots.txt. <strong>Подробности <a target="_blank" href="https://github.com/webitproff/sitemap-pages-xml-i18n-cotonti/blob/main/README.md"> в документации README.md</a></strong>.';
+
+// ========================
+// ЗАГОЛОВКИ И ОПИСАНИЯ (где те же значения, вытягиваются другими ключами)
+// ========================
+$L['sitemap_pages_title']       = $L['info_name'];
+$L['sitemap_pages_desc']        = $L['info_desc'];
 
 // ========================
 // ЧАСТОТЫ ОБНОВЛЕНИЯ (ОБЩИЕ)
@@ -61,7 +68,7 @@ $L['cfg_perpage_hint']       = 'Если страниц больше, карта
 
 // --- Языки ---
 $L['cfg_languages']          = 'Языки (через запятую)';
-$L['cfg_languages_hint']     = 'Например: en,ru,pl,ua. Оставьте пустым – используются все активные языки из конфигурации сайта.';
+$L['cfg_languages_hint']     = 'Например: en,ru,pl,ua. Оставьте пустым – используются все активные языки из конфигурации плагина i18n.';
 $L['cfg_default_lang']       = 'Язык по умолчанию (без префикса)';
 $L['cfg_default_lang_hint']  = 'Для этого языка карта будет доступна по адресу /sitemap-pages.xml без указания языкового префикса.';
 
